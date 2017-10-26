c9d390b1df887dbc17b81363700feb31be3eebc7
agb91 <andrea.damio1991@hotmail.it>
2017-10-26 20:34:24 +0200

    start build test cases

--- test_breeder.py
+++ test_breeder.py
@@ -14,7 +14,7 @@ class TestBreeder:
 		for i in range( 0, (nPop - 1) ):
 			gene = gen[i]
 			assert gene.isAcceptable() == True
-	
+	'''
 	def testNewGen(self):
 		nPop = 4
 		breeder = Breeder()
@@ -29,7 +29,7 @@ class TestBreeder:
 		assert len(newGen) == nPop - 2
 		newGen = breeder.getNewGeneration(gen , (nPop + 7) )
 		assert len(newGen) == nPop + 7
-	
+	'''
 	def testGetSon(self):
 		breeder = Breeder()
 		g1 = Gene( 1, 100, [1,2,3] )
@@ -57,4 +57,34 @@ class TestBreeder:
 			count += 1	
 		assert ( count == 1 )
 
+	def testOrderAndTakeGenes(self):
+		breeder = Breeder()
+		g1 = Gene( 1, 100, [1,2,3] )
+		g1.setFitnessLevel(5.2)
+		g2 = Gene( 1, 100, [1,2,3] )
+		g2.setFitnessLevel(15.8)
+		g3 = Gene( 1, 100, [1,2,3] )
+		g3.setFitnessLevel(12.83)
+		g4 = Gene( 1, 100, [1,2,3] )
+		g4.setFitnessLevel(3.23)
+		g5 = Gene( 1, 100, [1,2,3] )
+		g5.setFitnessLevel(2.15)
+		lg = [g1,g2,g3,g4,g5]
+		ordered = list()
+		ordered = breeder.orderGenes( lg )
+		assert( len(ordered) == len(lg) )
+		assert( ordered[0].level == 2.15 )
+		assert( ordered[1].level == 3.23 )
+		assert( ordered[2].level == 5.2 )
+		assert( ordered[3].level == 12.83 )
+		assert( ordered[4].level == 15.8 )
 
+		nGoods = 3 
+		goods = breeder.takeGoods( lg , nGoods )
+		assert( len(goods) == nGoods )
+		assert( goods[0].level == 2.15 )
+		assert( goods[1].level == 3.23 )
+		assert( goods[2].level == 5.2 )
+	
+		best = breeder.takeBest( lg )
+		assert( best.level == 2.15 )
